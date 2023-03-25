b178d279083d380459bec5c611371d251e8a8cda
josceno <jdamasceno414@gmail.com>
2023-03-25 11:26:15 -0300

    arrumnando main

--- src/Main.java
+++ src/Main.java
@@ -12,87 +12,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    Repositorio repositorio = new PostgresConnection();
-    Connection connection = repositorio.connectar("ETL-UNIVERSIDADE","postgres","changeme");
-    public  List<Diciplina> diciplinaList() throws SQLException {
-        List<Diciplina> diciplinaList = repositorio.disciplinas();
-        for (int i = 0; i < repositorio.disciplinas().size() ; i++) {
-            PreparedStatement preparedStatement = connection
-                    .prepareStatement("SELECT * FROM disciplinas");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
-                Diciplina equivalente = null;
-                Diciplina alvo = new Diciplina(0,0,"",null);
-                if(resultSet.getInt(4) >0){
-                    //System.out.println(resultSet.getInt(4));
-                    for (Diciplina eq: repositorio.disciplinas()) {
-                        if(eq.getCodigoDisciplina() == resultSet.getInt(4)){
-                            equivalente = eq;
-                        }
-                    }
-                    for (Diciplina alv: repositorio.disciplinas()) {
-                        if(alv.getCodigoDisciplina() == resultSet.getInt(1)){
-                            alvo = alv;
-                        }
-                    }
-                };
-
-                if(diciplinaList.get(i).getCodigoDisciplina() == alvo.getCodigoDisciplina()){
-                    diciplinaList.get(i).setEquivalente(equivalente);
-                }
-
-
-            }
-
-
-
-        }
-        return diciplinaList;
-
-    }
-    public  List<Professores> professoresList() throws SQLException {
-        List<Professores> professoresList = repositorio.professores();
-
-        for (Curso curso: repositorio.cursos()) {
-            for (int i = 0;  i<repositorio.professores().size(); i++) {
-                PreparedStatement preparedStatement =
-                        connection
-                                .prepareStatement("SELECT * FROM professores WHERE cod_curso = " +curso.getCodigoCurso());
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                while (resultSet.next()){
-                    if(resultSet.getInt(1) == repositorio.professores().get(i).getCodigoProfessor()){
-                        repositorio.professores().get(i).setCodigoCurso(curso);
-                        professoresList.get(i).setCodigoCurso(curso) ;}
-
-                }
-            }
-
-        }
-        return professoresList;
-    }
-    public static void main(String[] args) throws SQLException {
-        Repositorio repositorio = new PostgresConnection();
-
+    public static Repositorio repositorio = new PostgresConnection();
+    //Connection connection = repositorio.connectar("ETL-UNIVERSIDADE","postgres","changeme");
+    public static void main(String[] args) throws SQLException {;
 
         PostgresConnection prin = new PostgresConnection();
 
         for (int i = 0; i < 3; i++) {
             //repositorio.cursos().forEach(System.out::println);
-            prin.professoresList().forEach(System.out::println);
-            //prin.diciplinaList()
-            //repositorio.alunos().forEach(System.out::println);
-           repositorio.turmas().forEach(System.out::println);
-
-            /*for (Diciplina d: prin.diciplinaList()) {
-                   try {
-                       System.out.println(d);
-                   }catch (NullPointerException e){
-                       throw  e;
-
-                   }
-            }*/
-
+            //prin.professoresList().forEach(System.out::println);
+            prin.diciplinaList().forEach(System.out::println);
+            repositorio.alunos().forEach(System.out::println);
+           //repositorio.turmas().forEach(System.out::println);
         }
 
     }
--- src/repositorio/PostgresConnection.java
+++ src/repositorio/PostgresConnection.java
@@ -16,6 +16,37 @@ public class PostgresConnection implements Repositorio {
         professores();
 
     }
+    public  List<Diciplina> diciplinaList() throws SQLException {
+        List<Diciplina> diciplinaList = disciplinas();
+        for (int i = 0; i < disciplinas().size() ; i++) {
+            PreparedStatement preparedStatement = connection
+                    .prepareStatement("SELECT * FROM disciplinas");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                Diciplina equivalente = null;
+                Diciplina alvo = new Diciplina(0,0,"",null);
+                if(resultSet.getInt(4) >0){
+                    //System.out.println(resultSet.getInt(4));
+                    for (Diciplina eq: disciplinas()) {
+                        if(eq.getCodigoDisciplina() == resultSet.getInt(4)){
+                            equivalente = eq;
+                        }
+                    }
+                    for (Diciplina alv: disciplinas()) {
+                        if(alv.getCodigoDisciplina() == resultSet.getInt(1)){
+                            alvo = alv;
+                        }
+                    }
+                };
+
+                if(diciplinaList.get(i).getCodigoDisciplina() == alvo.getCodigoDisciplina()){
+                    diciplinaList.get(i).setEquivalente(equivalente);
+                }
+            }
+        }
+        return diciplinaList;
+
+    }
     public  List<Professores> professoresList() throws SQLException {
         List<Professores> professoresList = professores();
 
